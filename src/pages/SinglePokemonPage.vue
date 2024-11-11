f43ffa355c672b5797fda74b3bb5f01a02eb0370
Andrés Vargas <andres.pinneros@iepgroup.es>
2024-11-11 18:54:04 +0100

    cadena evolutiva

--- src/pages/SinglePokemonPage.vue
+++ src/pages/SinglePokemonPage.vue
@@ -14,37 +14,99 @@
             </template>
         </div>
     </div>
+    <div class="pokemon-line">
+        <template v-for="pokemonData in chainEvolution">
+            <pokemonBoxComponent :img="pokemonData.img" :name="pokemonData.name" :number="pokemonData.id"></pokemonBoxComponent>
+        </template>
+    </div>
     
 </template>
 
 <script setup>
+    import pokemonBoxComponent from '@/components/pokemonBoxComponent.vue';
     import { ref, onMounted } from 'vue';
     import { useRoute } from 'vue-router'
 
+    let pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/';
+    let chainEvolution = ref({});
     let route = useRoute(); 
     let id = ref(route.params.id);
 
     let pokemonData = ref({});
 
-    onMounted(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/" + route.params.id)
-        .then((data) => data.json())
+    let getIdFromUrl = (url) => {
+        let elements = url.split("/");
+
+        return elements[elements.length - 2];
+    };
+
+
+    let getPokemonLine = (pokemonData) => {
+
+        let pokemonLine = [];
+
+        pokemonLine.push(pokemonData.species);
+
+        if(pokemonData.evolves_to != []){
+            for(let i = 0 ; i < pokemonData.evolves_to.length ; i++){
+                pokemonLine.push(...getPokemonLine(pokemonData.evolves_to[i]));
+            }
+        }
+
+        return pokemonLine;
+    }
+
+
+    onMounted(async () => {
+        let data = await fetch(pokemonUrl + route.params.id);
+        data = await data.json();
+
+        //let speciesData = await fetch(data.species.url);
+        // speciesData = await speciesData.json();
+
+    
+
+        fetch(data.species.url).then(data => data.json())
         .then((data) => {
+            console.log(data.evolution_chain.url);
 
-            delete data.sprites.other;
-            delete data.sprites.versions;
+            fetch(data.evolution_chain.url).then(data => data.json())
+            .then((data) => {
+                
+                chainEvolution.value = getPokemonLine(data.chain);
 
-            pokemonData.value = data;
+                chainEvolution.value.forEach((value, key) => {
+                    let id =  getIdFromUrl(value.url);
 
-            pokemonData.value.moves.forEach((element, keyMove) => {
-                fetch(element.move.url)
-                .then(data => data.json())
-                .then((movesData) => {
-                    pokemonData.value.moves[keyMove].data = movesData;
+                    fetch(pokemonUrl + id)
+                    .then(data => data.json())
+                    .then((data) => {
+                        chainEvolution.value[key].img = data.sprites.front_default;
+                        chainEvolution.value[key].id = id;
+                    });
                 });
             });
-
         });
+
+        delete data.sprites.other;
+        delete data.sprites.versions;
+
+        pokemonData.value = data;
+
+        pokemonData.value.moves.forEach(async(element, keyMove) => {
+            
+            //data = await fetch(element.move.url);
+            let data = await fetch(element.move.url);
+            
+            data = await data.json();
+
+            pokemonData.value.moves[keyMove].data = data;
+        });
+
+        
+
+
+        /* cambiar por awaits */
     });
 
 
